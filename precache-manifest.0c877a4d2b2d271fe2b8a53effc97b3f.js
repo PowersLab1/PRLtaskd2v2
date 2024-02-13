0d48cbd5ff12d414e49e3e5001a200db2961399d
Max Greenwald <powerslab3@gmail.com>
2024-02-13 14:16:57 -0500

    Updates

---
+++ precache-manifest.0c877a4d2b2d271fe2b8a53effc97b3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35babd76d098810ce845a7bdd913b5ac",
+    "url": "/PRLtaskd2v2/index.html"
+  },
+  {
+    "revision": "9c689c4a97a69581c228",
+    "url": "/PRLtaskd2v2/static/css/main.f45e64f2.chunk.css"
+  },
+  {
+    "revision": "b53c36b08b69af92a9e7",
+    "url": "/PRLtaskd2v2/static/js/2.c52a2185.chunk.js"
+  },
+  {
+    "revision": "ba7279a4c883b4cf3de1f6b3c5a6bc14",
+    "url": "/PRLtaskd2v2/static/js/2.c52a2185.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c689c4a97a69581c228",
+    "url": "/PRLtaskd2v2/static/js/main.0b17a033.chunk.js"
+  },
+  {
+    "revision": "1a9510af05a88520bd3c",
+    "url": "/PRLtaskd2v2/static/js/runtime-main.f1c8e31c.js"
+  }
+]);
